684e888b31564bb141f752a4094fbf4b254a0fc6
Admin <admin@MacBook-Pro-di-Admin.local>
2025-04-10 12:28:55 +0200

    Added a simple set of tests to check a main button click event

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 
-import { render, screen } from '@testing-library/react';
+import { render, screen, within,fireEvent } from '@testing-library/react';
 import App from "./App";
 
-test("Render the login form", () => {
+test("Check if there isn't a 'button-clicked' text 'Clicked!'", () => {
     render(<App/>);
 
-    const loginForm = screen.getByTitle("main-container");
+    const buttonClickedMessage = screen.queryByTitle("button-clicked");
+    expect(buttonClickedMessage).toBeNull();
+});
+
+test("Check if there's a 'click-button' with a Tailwind class text-red-800 in the form 'main-form'", () => {
+    render(<App/>);
+
+    const loginForm = screen.getByTitle("main-form");
     expect(loginForm).toBeInTheDocument();
-    expect(loginForm).toHaveTextContent("Hello guys!");
-    expect(loginForm).toHaveClass("text-3xl font-bold text-blue-600");
+
+    const button = within(loginForm).getByTitle("click-button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass("text-red-800");
+});
+
+
+test("Check if there's a 'button-clicked' with the text 'Clicked!' when click the button", () => {
+    render(<App/>);
+
+    const button = screen.getByTitle("click-button");
+    fireEvent.click(button);
+
+    const buttonClickedMessage = screen.getByTitle("button-clicked");
+    expect(buttonClickedMessage).toBeInTheDocument();
+    expect(buttonClickedMessage).toHaveTextContent("Clicked!");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react'
+import { Button } from '@/components/ui/button';
 
 function App() {
-  return (
-    <div title='main-container' className="text-3xl font-bold text-blue-600">Hello guys!</div>
-  );
+    const [buttonClicked, setButtonClicked] = useState(false);
+
+    return (
+        <div title="main-form">
+            <Button title='click-button' className="text-red-800" onClick={() => setButtonClicked(true)}>
+                Click Me
+            </Button>
+
+            {buttonClicked && <div title="button-clicked">Clicked!</div>}
+        </div>
+    );
 }
 
 export default App
